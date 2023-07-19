bcd3318f55ca3e0a0a3743224e7d5e40e3fb2547
alexey <alexey@digger.dev>
2023-07-19 12:39:09 +0100

    remove secrets

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -1,7 +1,5 @@
 locals {
   cloud_web_secrets = [
-    { "key" : "JWT_PUBLIC_KEY", "value" : "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/cloud-infra/jwt-public-key" },
-    { "key" : "WEBHOOK_SECRET", "value" : "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/cloud-infra/webhook-secret" }
   ]
 }
 
